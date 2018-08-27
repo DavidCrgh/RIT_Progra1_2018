b795a4875df5fc51e657b4ff4f9f1f6d74410057
geraguti0807 <ger0807@hotmail.com>
2018-08-27 17:29:57 -0600

    Cambios

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using ConsoleApp1.lectores;
 
+using ConsoleApp1.lectores;
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Database database = new Database();
+            /*Database database = new Database();
 
             string text1 = "Costa Rica denominada oficialmente República de Costa Rica" +
                 " es una nación soberana organizada como una república presidencialista unitaria compuesta por 7 provincias";
@@ -41,9 +43,29 @@ namespace ConsoleApp1
 
             database.Set_diccionary();
 
+<<<<<<< HEAD
             database.Set_words_per_doc();
 
             //database.print();
+=======
+            Console.WriteLine(database.Get_lenght_dic().ToString());*/
+
+            string pathArchivo = Console.ReadLine();
+            string contenido = LectorColeccion.ObtenerContenidoArchivo(pathArchivo);
+
+            contenido = ServiciosRegex.RemoverFormato_Comentarios(contenido);
+            contenido = ServiciosRegex.ReemplazarParametros(contenido);
+            contenido = Scrubber.Remove_acentos(contenido);
+
+            var listaTerminos = ServiciosRegex.TokenizarMatches(contenido);
+
+            Console.WriteLine(contenido);
+            Console.WriteLine("\nlistaTerminos=");
+            foreach(var t in listaTerminos)
+            {
+                Console.WriteLine(t);
+            }
+>>>>>>> b97e9cdbe4b03729d62ecc6a1e338ea25e109703
 
             Console.ReadKey();
         }
