569afc212eec6ddadc2a835b4285a494c004255d
geraguti0807 <ger0807@hotmail.com>
2018-09-08 17:03:13 -0600

    program update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using ConsoleApp1.lectores;
+using ConsoleApp1.escritores;
 using ConsoleApp1.algoritmos;
 
 namespace ConsoleApp1
@@ -37,9 +38,50 @@ namespace ConsoleApp1
 
             vectorial.Make_Scale();
 
+<<<<<<< HEAD
             vectorial.scale.print_Scale();*/
 
             //Okapi_BM25 bm25 = new Okapi_BM25(indice);
+=======
+            //vectorial.scale.print_Scale();
+
+            /*string pathArchivo = Console.ReadLine();
+            string contenido = LectorColeccion.ObtenerContenidoArchivo_Raw(pathArchivo);
+
+            contenido = ServiciosRegex.RemoverFormato_Comentarios(contenido);
+            contenido = ServiciosRegex.ReemplazarParametros(contenido);
+            contenido = Scrubber.Remove_acentos(contenido);
+
+            var listaTerminos = ServiciosRegex.TokenizarMatches(contenido);
+
+            Console.WriteLine(contenido);
+            Console.WriteLine("\nlistaTerminos=");
+            foreach(var t in listaTerminos)
+            {
+                Console.WriteLine(t);
+            }*/
+
+            string pathColeccion = "C:\\Users\\davva\\Desktop\\RIT_P1\\man-es";
+            string pathStopwords = "";
+            string pathIndice = "";
+
+            Database indice = Indexer.IndexarColeccion(pathColeccion, pathStopwords, pathIndice);
+
+            string consulta = "segmentos de memoria compartida";
+
+            Indexer.IndexarQuery(consulta, indice);
+
+            Okapi_BM25 bm25 = new Okapi_BM25(indice);
+
+            bm25.CrearEscalafonBM25();
+
+            string pathEscalafonTexto = "C:\\Users\\davva\\Desktop\\escalafon.txt";
+            string pathEscalafonHTML = "C:\\Users\\davva\\Desktop\\escalafon.html";
+
+            EscritorEscalafon escritor = new EscritorEscalafon(indice.Get_doc_info(), bm25.scale, 30);
+            escritor.Escribir_Texto(pathEscalafonTexto);
+            escritor.Escribir_HTML(pathEscalafonHTML);
+>>>>>>> 64238337da80a6c8e90895b04b1d6d4b8667da6a
 
             //bm25.CrearEscalafonBM25();
           
