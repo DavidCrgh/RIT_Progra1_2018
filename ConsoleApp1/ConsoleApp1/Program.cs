fdc3dd3486ea0db06a51db5ce88568378dec2b04
geraguti0807 <ger0807@hotmail.com>
2018-09-08 19:41:11 -0600

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,20 +24,20 @@ namespace ConsoleApp1
 
             Indexer.IndexarQuery(consulta, indice);
 
-            Okapi_BM25 bm25 = new Okapi_BM25(indice);
+            /*Okapi_BM25 bm25 = new Okapi_BM25(indice);
 
-            bm25.CrearEscalafonBM25();
+            bm25.CrearEscalafonBM25();*/
 
             Vectorial vectorial = new Vectorial(indice);
 
             vectorial.Compare_Query_Docs();
 
-            string pathEscalafonTexto = "C:\\Users\\davva\\Desktop\\escalafon.txt";
+            /*string pathEscalafonTexto = "C:\\Users\\davva\\Desktop\\escalafon.txt";
             string pathEscalafonHTML = "C:\\Users\\davva\\Desktop\\escalafon.html";
 
             EscritorEscalafon escritor = new EscritorEscalafon(indice.Get_doc_info(), bm25.scale, 30);
             escritor.Escribir_Texto(pathEscalafonTexto);
-            escritor.Escribir_HTML(pathEscalafonHTML);
+            escritor.Escribir_HTML(pathEscalafonHTML);*/
 
             //bm25.CrearEscalafonBM25();
           
